35ed8f38ba5da2e0353d0b77d8be74b8544fcc91
3Lightwave25 <itslightningwave@gmail.com>
2026-07-26 14:43:32 +0530

    Class 2 classwork

---
+++ Lesson2/Activity1.py
@@ -0,0 +1,4 @@
+print("6+23=: ",6+23)
+print("Python is the best programming language in the world",end="!")
+print("You can also skip a line like this:\n")
+print("Look so cool")
---
+++ Lesson2/Activity2.py
@@ -0,0 +1,11 @@
+n1="Hello"
+print(n1)
+_we="Hello"
+print(_we)
+#Hello this wont work 
+#print("High")
+print("#")
+n1=input("Hello what is your name?: ")
+print(n1,"Is a nice name!!")
+n2=int(input("What is your age?: "))
+print("Nice age",n2)
