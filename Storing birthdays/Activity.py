ed9dd85ec60414f4482e6bf6badca2f8f3d9c8ac
3Lightwave25 <itslightningwave@gmail.com>
2026-08-02 11:38:23 +0530

    Activity storing bdays

---
+++ Storing birthdays/Activity.py
@@ -0,0 +1,12 @@
+n1 = input("Enter your name: ")
+n2 = input("Enter your personal goal: ")
+n3 = input("Enter the target month: ")
+n4 = 30
+ 
+
+print("\nName: ", n1)
+print("Goal: ", n2)
+print("Target Month: ", n3)
+print("Daily Practice: ", n4, "minutes")
+ 
+
